dc19fcca1e473a7d5f788bf8d34948b4ab2c01bc
aquito <aquito@live.cn>
2025-09-16 19:51:10 +0800

    Linux 缺少libiohook

--- mainwindow/mainwindow.h
+++ mainwindow/mainwindow.h
@@ -6,33 +6,37 @@
 
 #include <string>
 
-#include <QUrl>
-#include <QFile>
-#include <QWidget>
-#include <iostream>
-#include <QKeyEvent>
-#include <QFileInfo>
 #include <QClipboard>
+#include <QFile>
+#include <QFileInfo>
 #include <QJsonObject>
+#include <QKeyEvent>
+#include <QListWidget>
 #include <QMainWindow>
 #include <QMessageBox>
-#include <QListWidget>
-#include <QNetworkReply>
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QUrl>
+#include <QWidget>
+#include <iostream>
 
+#include "KeyBoardMouseHook/KeyBoardMouseHook.h"
 #include "Public_Func/PublicFunc.h"
 #include "network_class/NetworkClass.h"
-#include "KeyBoard_Mouse_Hook/KeyBoard_Mouse_Hook.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow;}
+namespace Ui
+{
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow, public Pubilc_Func{
-Q_OBJECT
+class MainWindow : public QMainWindow, public Pubilc_Func
+{
+    Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget* parent = nullptr);
 
     ~MainWindow() override;
 
@@ -46,17 +50,17 @@ signals:
 
 
 public:
-    QUrl *setUrl();
+    QUrl* setUrl();
 
     QUrl setUrl(bool point);
 
-//    void keyPressEvent(QKeyEvent *event) override;
+    //    void keyPressEvent(QKeyEvent *event) override;
 
     void addListWidget();
 
     void delListWidget();
 
-    void insertListWidget(const QString &input, int row);
+    void insertListWidget(const QString& input, int row);
 
     void refreshListWidget();
 
@@ -71,29 +75,29 @@ public:
 private slots:
     void on_pushButton_clicked(bool checked);
 
-    QString getRcvBuf(emitBundle &emitBundle);
+    QString getRcvBuf(emitBundle& emitBundle);
 
-    void on_listWidget_itemDoubleClicked(QListWidgetItem *item);
+    void on_listWidget_itemDoubleClicked(QListWidgetItem* item);
 
 private:
     void setText();
 
 private:
     static MainWindow* m_mainWindow;
-    KeyBoard_Mouse_Hook m_KBMH;             //全局监控类
-    QUrl *m_Url{};                          //Url
-    QFile *m_File{};                        //用于读取配置文件
-    Ui::MainWindow *m_ui;                   //UI对象
-    QNetworkReply *m_Reply{};               //网络应答
-    QClipboard *m_ClipBoard{};              //获取系统粘贴板
-    QJsonObject m_JsonObj;                  //读取和解析配置文件
-    bool m_Hide_UI;                         //
-    bool m_DownloadFlag{};                  //
-    bool m_UploadFlag{};                    //
-    unsigned int m_ReconnectTimes;          //重连次数
-    std::map<int, std::string> *m_record{}; //用于管理粘贴板的历史记录
+    KeyBoardMouseHook m_KBMH;              //全局监控类
+    QUrl* m_Url{};                         //Url
+    QFile* m_File{};                       //用于读取配置文件
+    Ui::MainWindow* m_ui;                  //UI对象
+    QNetworkReply* m_Reply{};              //网络应答
+    QClipboard* m_ClipBoard{};             //获取系统粘贴板
+    QJsonObject m_JsonObj;                 //读取和解析配置文件
+    bool m_Hide_UI;                        //
+    bool m_DownloadFlag{};                 //
+    bool m_UploadFlag{};                   //
+    unsigned int m_ReconnectTimes;         //重连次数
+    std::map<int, std::string>* m_record{};//用于管理粘贴板的历史记录
 };
 
-#endif // FTP_CLIPBOARD_MAINWINDOW_H
+#endif// FTP_CLIPBOARD_MAINWINDOW_H
 
 #pragma clang diagnostic pop
--- network_class/NetworkClass.h
+++ network_class/NetworkClass.h
@@ -2,48 +2,51 @@
 #define NETWORK_CLASS_H
 #pragma clang diagnostic push
 #pragma clang diagnostic push
-#pragma ide diagnostic ignored "OCInconsistentNamingInspection"
+// #pragma ide diagnostic ignored "OCInconsistentNamingInspection"
 #pragma clang diagnostic push
-#pragma ide diagnostic ignored "NotImplementedFunctions"
-#pragma ide diagnostic ignored "google-explicit-constructor"
+// #pragma ide diagnostic ignored "NotImplementedFunctions"
+// #pragma ide diagnostic ignored "google-explicit-constructor"
 
-#include <QUrl>
-#include <QDebug>
-#include <QThread>
-#include <QClipboard>
 #include <QByteArray>
-#include <QMessageBox>
-#include <QNetworkReply>
+#include <QClipboard>
+#include <QDebug>
 #include <QListWidgetItem>
+#include <QMessageBox>
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QThread>
+#include <QUrl>
 
-#include "file_class/file_class.h"
+#include "FileClass/FileClass.h"
 #include "Public_Func/PublicFunc.h"
 
-class networkClass : public QThread ,public Pubilc_Func
+class networkClass : public QThread, public Pubilc_Func
 {
     Q_OBJECT
 
 signals:
     void sendSignal(emitBundle buf);
+
 public:
-    enum status {
-        THREAD_START, THREAD_STOP
+    enum status
+    {
+        THREAD_START,
+        THREAD_STOP
     };
 
     networkClass();
 
-    networkClass(const QUrl &url);
+    networkClass(const QUrl& url);
 
-    networkClass(QUrl *url);
+    networkClass(QUrl* url);
 
     [[noreturn]] void run();
 
     //QString 上次到服务器
 
-    void setUrl(const QUrl &url);
+    void setUrl(const QUrl& url);
 
-    void setUrl(QUrl *url);
+    void setUrl(QUrl* url);
 
 public slots:
     QString getRcvBuf(emitBundle emitBundle);
@@ -54,9 +57,9 @@ public slots:
 
     bool replyError(QNetworkReply::NetworkError);
 
-    bool finishedOfUpload(QNetworkReply *ftpReply);
+    bool finishedOfUpload(QNetworkReply* ftpReply);
 
-    static bool finishedOfDownload(QNetworkReply *ftpReply);
+    static bool finishedOfDownload(QNetworkReply* ftpReply);
 
     void changeThreadStatus(bool status);
 
@@ -67,32 +70,30 @@ public slots:
     static networkClass* instance();
 
 private:
-    QNetworkAccessManager *getInstance();
+    QNetworkAccessManager* getInstance();
 
-    bool send(const QString &sndStr2Serv, const QUrl &url);
+    bool send(const QString& sndStr2Serv, const QUrl& url);
 
-    bool sync(const QUrl &url);
+    bool sync(const QUrl& url);
 
     bool isInit();
 
 
-
 private:
+    static networkClass* m_networkClass;
 
-    static networkClass *m_networkClass;
-
-    QUrl *m_url{};
-    QString m_swpBuf;                       //用于去除重复上下载操作
-    bool m_operator_Mutex;                  //上下载操作锁   TODO 其他的上下载操作放入队列中
-    bool m_cbChange{};                      //判断粘贴板内容改变
-    bool m_clsIsInit;                       //本类是否被初始化过
-    bool m_ThreadStatus{};                  //线程状态
-    int m_reconnectTimes{};                 //重连次数
-    QNetworkReply *m_reply{};               //应答类
-    File_Class *m_file_class{};             //文件管理
-    QNetworkAccessManager *m_manager{};     //网络请求类
+    QUrl* m_url{};
+    QString m_swpBuf;                  //用于去除重复上下载操作
+    bool m_operator_Mutex;             //上下载操作锁   TODO 其他的上下载操作放入队列中
+    bool m_cbChange{};                 //判断粘贴板内容改变
+    bool m_clsIsInit;                  //本类是否被初始化过
+    bool m_ThreadStatus{};             //线程状态
+    int m_reconnectTimes{};            //重连次数
+    QNetworkReply* m_reply{};          //应答类
+    File_Class* m_file_class{};        //文件管理
+    QNetworkAccessManager* m_manager{};//网络请求类
 };
 
-#endif // NETWORK_CLASS_H
+#endif// NETWORK_CLASS_H
 #pragma clang diagnostic pop
 #pragma clang diagnostic pop
